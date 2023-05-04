48782f51b421be5f05e892349780400be100e787
luciano-mc <luciano.martiin@gmail.com>
2023-05-04 12:55:21 +0200

    V 1.0

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Watch for the scroll to set CSS classes
+const animatedElements = document.querySelectorAll('[data-animated]')
+let height = window.innerHeight/1.5;
+
+window.addEventListener('scroll', () => {
+    animatedElements.forEach(element => {
+        let sectionTop = element.getBoundingClientRect().top;
+        if (sectionTop < height) {
+            let animationName = element.dataset.animated;
+            element.classList.add(animationName);
+        }
+    })
+})
+
+// Set the color theme
+let theme = document.getElementById('theme');
+let colorModeButton = document.querySelector('.color__mode');
+let languageModeButton = document.querySelector('.language__mode');
+
+colorModeButton.addEventListener('click', () => {
+    colorModeButton.dataset.color === 'dark-mode' ? colorModeButton.setAttribute('data-color', 'light-mode') : colorModeButton.setAttribute('data-color', 'dark-mode');
+    switch (colorModeButton.dataset.color) {
+        case 'light-mode': theme.href = 'css/lightStyle.css';
+        console.log(theme)
+        break;
+
+        case 'dark-mode': theme.href = 'css/darkStyle.css';
+        console.log(theme);
+        break;
+    }
+})
+
+// Closes menu when click on link
+let navbar = document.querySelector('.hero__nav');
+let checkButton = document.querySelector('#check');
+
+navbar.addEventListener('click', (e) => {
+    if (e.target.classList.contains('hero__nav-link')) {
+        checkButton.checked = false;
+    } 
+})
